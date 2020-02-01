0abfa6a6e4ad31e4922769e190a1b5f829cd724d
Emma Kang <emma.kang@ubixeng.com>
2020-02-01 00:49:58 -0500

    replace total frequent renter point

--- Java/src/com/ekang/refactoring/chapter1/Customer.java
+++ Java/src/com/ekang/refactoring/chapter1/Customer.java
@@ -34,11 +34,21 @@ public class Customer {
         }
         // add footer lines
         result += "Amount owed is " + String.valueOf(getTotalCharge()) + "\n";
-        result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points";
+        result += "You earned " + String.valueOf(getTotalFrequentRenterPoints()) + " frequent renter points";
 
         return result;
 
         }
+
+    private int getTotalFrequentRenterPoints() {
+        int result = 0;
+        Enumeration rentals = _rentals.elements();
+        while(rentals.hasMoreElements()) {
+            Rental each = (Rental) rentals.nextElement();
+            result += each.getFrequentRenterPoints();
+        }
+        return result;
+    }
 
     private double getTotalCharge() {
         double result = 0;
