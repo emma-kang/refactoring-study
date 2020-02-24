e35cbf9bf40ca545508f0eda4d8dc4744ff066b7
Emma Kang <emma140626@gmail.com>
2020-02-23 21:20:39 -0500

    add inline temp

--- Java/src/com/ekang/refactoring/chapter6/InlineMethod.java
+++ Java/src/com/ekang/refactoring/chapter6/InlineMethod.java
@@ -2,10 +2,18 @@ package com.ekang.refactoring.chapter6;
 
 public class InlineMethod {
     private int _numberOfLateDeliveries;
+    private Order anOrder;
+
+    public boolean inlineTemp() {
+        double basePrice = anOrder.basePrice();
+        return (basePrice > 1000);
+    }
 
     public int getRating() {
         return (_numberOfLateDeliveries > 5) ? 2 : 1;
     }
+
+
 
     /*
      * [Inline Method]
@@ -13,6 +21,6 @@ public class InlineMethod {
      * Find all calls to the method
      * Replace each call with the method body
      * Complie and test
-     * Remove the method definition 
+     * Remove the method definition
      */
 }
--- Java/src/com/ekang/refactoring/chapter6/Order.java
+++ Java/src/com/ekang/refactoring/chapter6/Order.java
@@ -3,6 +3,7 @@ package com.ekang.refactoring.chapter6;
 
 public abstract class Order {
     abstract double getAmount();
+    abstract double basePrice();
 
 }
 
