c1c41f77c59118ddc88d2873a800263eb0b79204
Emma Kang <emma140626@gmail.com>
2020-02-24 23:24:13 -0500

    add example 'remove assignment to parameter'

---
+++ Java/src/com/ekang/refactoring/chapter6/RemoveAssignToParam.java
@@ -0,0 +1,26 @@
+package com.ekang.refactoring.chapter6;
+/*
+ * If you pass in an object named foo, in the parameter, assigning to the parameter
+ * means to change foo to refer to a different object.
+ *
+ * The lack of clarity and to confusion between
+ * pass by value and pass by reference
+ * foo.modifyInSomeWay(); // OK
+ * foo = anotherObject; // trouble
+ *
+ * Create a temporary variable for the parameter
+ * Replace all references to the parameter, made after the assignment, to the temporary variable.
+ * Change the assignment to assign to the temporary variable
+ * Compile and test
+ */
+
+public class RemoveAssignToParam {
+
+    public int discount(int inputVal, int quantity, int yearToDate) {
+        if (inputVal > 50) inputVal -= 2;
+        if (quantity > 100) inputVal -= 1;
+        if (yearToDate > 10000) inputVal -= 4;
+
+        return inputVal;
+    }
+}
