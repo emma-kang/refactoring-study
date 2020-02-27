ffb152e3dc66fad52dc3db56cde6c93daf5288ba
Emma Kang <emma140626@gmail.com>
2020-02-26 22:35:26 -0500

    add example 'Replace Method with Method obj'

--- Java/src/com/ekang/refactoring/chapter6/Order.java
+++ Java/src/com/ekang/refactoring/chapter6/Order.java
@@ -1,9 +1,20 @@
 package com.ekang.refactoring.chapter6;
 
-
+// have a long method that uses local variables in such a way that you cannot apply Extract Method
 public abstract class Order {
     abstract double getAmount();
     abstract double basePrice();
+
+    double price() {
+        double primaryBasePrice;
+        double secondaryBasePrice;
+        double tertiaryBasePrice;
+        double result = 0;
+
+        // long computation
+        // ...
+        return result;
+    }
 
 }
 
