0f8787482f09bd0b05c38dbe3aab1538570ccbad
Emma Kang <emma140626@gmail.com>
2020-02-26 22:59:57 -0500

    replace with a new algorithm

--- Java/src/com/ekang/refactoring/chapter6/SubstituteAlgorithm.java
+++ Java/src/com/ekang/refactoring/chapter6/SubstituteAlgorithm.java
@@ -1,20 +1,18 @@
 package com.ekang.refactoring.chapter6;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class SubstituteAlgorithm {
     // if you want to replace an algorithm with one that is clearer
     // Replace the body of the method with the new algorithm
 
     String foundPerson(String[] people) {
-        for (int i = 0; i < people.length; i++) {
-            if (people[i].equals("Don")) {
-                return "Don";
-            }
-            if (people[i].equals("John")) {
-                return "John";
-            }
-            if (people[i].equals("Kent")) {
-                return "Kent";
-            }
+        List candidates = Arrays.asList(new String[] {"Don", "John", "Kent"});
+
+        for(int i = 0; i < people.length; i++) {
+            if (candidates.contains(people[i]))
+                return people[i];
         }
 
         return "";
