77a30c961f7e14816746a3224032eda992b770bf
Emma Kang <yuseon1216@gmail.com>
2020-05-17 16:04:35 -0400

    add 1st step move field

--- Java/src/com/ekang/refactoring/chapter7/Person.java
+++ Java/src/com/ekang/refactoring/chapter7/Person.java
@@ -9,18 +9,20 @@ public class Person {
     private String _name;
     private String _officeAreaCode;
     private String _officeNumber;
+    private TelephoneNumber _officeTelephone = new TelephoneNumber();
 
     public String getName() {
         return _name;
     }
+    // Let's extract the telephone number
     public String getTelephoneNumber() {
-        return ("(" + _officeAreaCode + ")" + _officeNumber);
+        return ("(" + getOfficeAreaCode() + ")" + _officeNumber);
     }
     String getOfficeAreaCode() {
-        return _officeAreaCode;
+        return _officeTelephone.getAreaCode();
     }
     void setOfficeAreaCode(String arg) {
-        _officeAreaCode = arg;
+        _officeTelephone.setAreaCode(arg);
     }
     String getOfficeNumber() {
         return _officeNumber;
---
+++ Java/src/com/ekang/refactoring/chapter7/TelephoneNumber.java
@@ -0,0 +1,12 @@
+package com.ekang.refactoring.chapter7;
+
+public class TelephoneNumber {
+    private String _areaCode;
+
+    String getAreaCode() {
+        return _areaCode;
+    }
+    void setAreaCode(String arg) {
+        _areaCode = arg;
+    }
+}
