48552074adcb6f1fb883eff2af77fb91a9dffeb6
Emma Kang <yuseon1216@gmail.com>
2020-06-03 17:51:31 -0400

    ch8 apply self encapsulate field

--- Java/src/com/ekang/refactoring/chapter8/Range.java
+++ Java/src/com/ekang/refactoring/chapter8/Range.java
@@ -3,6 +3,14 @@ package com.ekang.refactoring.chapter8;
 class Range {
     private int low, high;
     boolean includes(int arg) {
-        return arg >= low && arg <= high;
+        return arg >= getLow() && arg <= getHigh();
+    }
+
+    // Self Encapsulate field - create getter and setter for the fields
+    int getLow() {
+        return low;
+    }
+    int getHigh() {
+        return high;
     }
 }
