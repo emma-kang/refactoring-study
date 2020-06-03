ba82b25678423c309a3211f3716c951c097b188f
Emma Kang <yuseon1216@gmail.com>
2020-06-03 18:13:45 -0400

    ch8 apply self-encapsulate field

--- Java/src/com/ekang/refactoring/chapter8/IntRange.java
+++ Java/src/com/ekang/refactoring/chapter8/IntRange.java
@@ -1,9 +1,19 @@
 package com.ekang.refactoring.chapter8;
 
+/**
+ * Be careful to use setting method in constructor 
+ * Often it is assumed that use the setting method for changes 
+ * after the object is created 
+ */
+
 public class IntRange {
     private int low, high;
 
     IntRange (int low, int high) {
+        initialize(low, high);
+    }
+
+    private void initialize (int low, int high) {
         this.low = low;
         this.high = high;
     }
@@ -13,6 +23,22 @@ public class IntRange {
     }
 
     void grow(int factor) {
-        high = high * factor;
+        setHigh(getHigh() * factor);
+    }
+
+    int getLow() {
+        return low;
+    }
+
+    int getHigh() {
+        return high;
+    }
+
+    void setLow(int arg) {
+        low = arg;
+    }
+
+    void setHigh(int arg) {
+        high = arg;
     }
 }
